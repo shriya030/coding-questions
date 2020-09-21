302e31b1f4a5ef99d5a2d6b3aee19250ca01664d
Shriya <56861600+shriya030@users.noreply.github.com>
2020-09-21 20:54:15 +0530

    Create Digit-frequency.py

---
+++ Arrays/Digit-frequency.py
@@ -0,0 +1,5 @@
+https://www.pepcoding.com/resources/online-java-foundation/function-and-arrays/digit-frequency-official/ojquestion#
+
+n = str(int(input()))
+d = str(int(input()))
+print(n.count(d))
