57a17c1d5a4821f3a5f55b417c79373d8ebddb03
Shriya <56861600+shriya030@users.noreply.github.com>
2020-09-27 13:25:59 +0530

    Update reverse-integer.cpp

--- Math/reverse-integer.cpp
+++ Math/reverse-integer.cpp
@@ -7,9 +7,9 @@ public:
     int reverse(int x) {
         int rev = 0;
         while (x) {
-            if (rev > INT_MAX / 10 ||rev < INT_MIN / 10)
-                return 0;
             int rem = x % 10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && rem > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && rem < -8)) return 0;
             rev = (rev * 10) + rem;
             x /= 10;
         }
