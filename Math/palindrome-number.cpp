921c8a9b5732d1dd23cecf238b7050a667eee946
Shriya <56861600+shriya030@users.noreply.github.com>
2020-09-27 16:11:56 +0530

    Create palindrome-number.cpp

---
+++ Math/palindrome-number.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/palindrome-number/
+
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x == 0)
+            return true;
+        if ( x < 0 || x % 10 == 0)
+            return false;
+        else {
+            int rem, rev = 0, temp = x;
+        while (x > 0) {
+            rem = x % 10;
+            // overflow condition
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && rem > 7)) return false;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && rem < -8)) return false;
+            rev = (rev * 10) + rem;
+            x /= 10;
+        }
+        return temp == rev;
+        }
+    }
+};
