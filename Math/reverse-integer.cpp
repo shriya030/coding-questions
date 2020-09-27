56b38fb9d8db3e0b748e4b4a05452d6a3a9918fc
Shriya <56861600+shriya030@users.noreply.github.com>
2020-09-27 16:16:51 +0530

    Update reverse-integer.cpp

--- Math/reverse-integer.cpp
+++ Math/reverse-integer.cpp
@@ -1,5 +1,16 @@
 https://leetcode.com/problems/reverse-integer
 
+/*
+To explain, lets assume that \text{rev}rev is positive.
+1. If temp = rev * 10 + pop causes overflow, then it must be that rev ≥ INT_MAX/10.
+2. If rev> INT_MAX/10, then temp = rev * 10 + pop is guaranteed to overflow.
+3. If rev == INT_MAX/10, then temp = rev * 10 + pop will overflow if and only if pop>7.
+Similar logic can be applied when \text{rev}rev is negative.
+*/
+
+// if (rev > INT_MAX/10 || rev < INT_MIN/10) return false;
+// This can also be used as an overflow condition
+
 
 
 class Solution {
